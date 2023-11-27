7e07d293e78c78beab87432a86fdc0f0bea84a4f
HuShuhan <873933169@qq.com>
2023-11-27 14:19:44 +0800

    DragGAN GUI, backend already commit in last commit

--- gui.py
+++ gui.py
@@ -135,3 +135,108 @@ dpg.start_dearpygui()
 dpg.destroy_context()
 
 
+# -----------------------------------------------------------------------------
+
+
+# global parameters
+add_point = 0 
+point_color = [(1, 0, 0), (0, 0, 1)] # 起始点为红色，目标点为蓝色
+points, steps = [], 0
+dragging = False
+
+# 每点击一次add point按钮，可以在图片上指定一组起始点和目标点
+def select_point(sender, app_data):
+    global add_point, points
+    if add_point <= 0: return
+    ms_pos = dpg.get_mouse_pos(local=False)
+    id_pos = dpg.get_item_pos('image_data')
+    iw_pos = dpg.get_item_pos('Image Win')
+    ix = int(ms_pos[0]-id_pos[0]-iw_pos[0])
+    iy = int(ms_pos[1]-id_pos[1]-iw_pos[1])
+    draw_point(ix, iy, point_color[add_point % 2])
+    points.append(np.array([ix, iy]))
+    print(points)
+    add_point -= 1
+
+# 在图像上显示用户选定的点
+def draw_point(x, y, color):
+    x_start, x_end = max(0, x - 2), min(image_width, x + 2)
+    y_start, y_end = max(0, y - 2), min(image_height, y + 2)
+    for x in range(x_start, x_end):
+        for y in range(y_start, y_end):
+            offset = (y * image_width + x) * rgba_channel
+            raw_data[offset:offset + rgb_channel] = array('f', color[:rgb_channel])
+
+# 绑定回调函数
+with dpg.item_handler_registry(tag='double_clicked_handler'):
+    dpg.add_item_double_clicked_handler(callback=select_point)
+dpg.bind_item_handler_registry("image_data", "double_clicked_handler")
+
+# drag窗口的位置
+drag_posx, drag_posy = posx, posy + height + 2
+with dpg.window(
+    label='Drag', width=width, height=height, pos=(drag_posx, drag_posy),
+    no_move=True, no_close=True, no_collapse=True, no_resize=True,
+):
+    # 点击 添加point按钮
+    def add_point_callback():
+        global add_point
+        add_point += 2
+    # 点击重置按钮
+    def reset_point_callback():
+        global points
+        points = []
+    # 点击开始drag
+    def start_callback():
+        global dragging
+        if dragging: return # 如果正在drag, 则直接退出
+        dragging = True
+
+    # 点击停止drag
+    def stop_callback():
+        global dragging
+        dragging = False
+    
+    dpg.add_text('drag', pos=(5, 20))
+    # 增加point
+    dpg.add_button(label="add point", width=80, pos=(70, 20), callback=add_point_callback)
+    # 重置point
+    dpg.add_button(label="reset point", width=80, pos=(155, 20), callback=reset_point_callback)
+    # 开始运行drag过程
+    dpg.add_button(label="start", width=80, pos=(70, 40), callback=start_callback)
+    # 手动停止drag过程
+    dpg.add_button(label="stop", width=80, pos=(155, 40), callback=stop_callback)
+    # 动态显示当前已经迭代的步数
+    dpg.add_text('steps: 0', tag='steps', pos=(70, 60))
+
+def dragging_thread():
+    global points, steps, dragging
+    # 将用户指定的点分成初始点和目标点
+    init_pts = []
+    tar_pts = []
+    for i in range(0, len(points), 2):
+        init_pts.append(points[i])
+        tar_pts.append(points[i+1])
+    init_pts = np.vstack(init_pts)[:, ::-1].copy()
+    tar_pts = np.vstack(tar_pts)[:, ::-1].copy()
+    
+    # 准备迭代优化
+    model.prepare_to_drag(init_pts)
+
+    while (dragging):
+        # 迭代一次
+        status, ret = model.drag(init_pts, tar_pts)
+        if status:
+            init_pts, _, image = ret
+        else:
+            dragging = False
+            return
+        # 显示最新的图像
+        update_image(image)
+        for i in range(init_pts.shape[0]):
+            draw_point(int(init_pts[i][1]), int(init_pts[i][0]), point_color[0])
+        for i in range(tar_pts.shape[0]):
+            draw_point(int(tar_pts[i][1]), int(tar_pts[i][0]), point_color[1])
+
+        steps += 1
+        dpg.set_value('steps', f'steps: {steps}')
